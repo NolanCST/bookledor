20552889e146e1585bfea795ef2e24ba2a75a4ed
AndreaBGLR <andrea.baglieri@laposte.net>
2023-11-27 11:19:12 +0100

    crea index, create

---
+++ resources/views/book/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Bibliothèque Bookldor')
+
+@section('content')
+
+
+    <ol>
+        @foreach($books as $book)
+            <li>
+                <a href="{{route('book.show', $post)}}"><i class="fa-solid fa-magnifying-glass"></i>{{$book['title']}}</a>
+                {{-- <a href="{{route('post.edit', $post)}}"><i class="fa-solid fa-pencil"></i></a> --}}
+                {{-- {{$blog}}  --}}
+            </li>
+        @endforeach
+    </ol>
+@endsection
