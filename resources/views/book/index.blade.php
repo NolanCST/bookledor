34670a83bc91245d87cdc7661d68de595cf057e0
AndreaBGLR <andrea.baglieri@laposte.net>
2023-12-01 14:34:16 +0100

    details

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -10,17 +10,6 @@
         .sm\:justify-between{
             justify-content: center
         }
-
-        @media (max-width: 650px) {
-
-        /* span.relative.inline-flex.items-center.px-4.py-2.text-sm.font-medium.text-gray-500.bg-white.border.border-gray-300.cursor-default.leading-5.rounded-md {
-            display: none;
-        }
-        div.flex.justify-between.flex-1.sm\:hidden {
-        display: none;
-        } */
-
-    }
     </style>
         <div class="component">
             <ul class="align">
