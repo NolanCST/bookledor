88d58630999c6b377bdd15421969c645579353c9
AndreaBGLR <andrea.baglieri@laposte.net>
2023-12-01 17:50:30 +0100

    reparation route create

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(route('book.index'));
+    return view(route('book.index'));
 });
 Route::get('/search', [BookController::class, 'search']);
 
@@ -29,12 +29,11 @@ Route::get('/dashboard', function () {
 // Route::get('/only-verified', function () {
 //     return view('only-verified');
 //  })->middleware(['auth', 'verified']);
-Route::get('/book', [BookController::class, 'index'])->name('book.index');
-Route::get('/book/{book}', [BookController::class, 'show'])->name('book.show');
+
 
 Route::middleware('auth')->group(function () {
 
-    Route::resource('book',BookController::class)->except(['index', 'show']);
+    Route::resource('book',BookController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
