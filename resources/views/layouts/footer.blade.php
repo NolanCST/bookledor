4aa509f16c6f4bf564216ec0d180577532e2661c
AndreaBGLR <andrea.baglieri@laposte.net>
2023-12-01 11:34:19 +0100

    perfectionnement du footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,11 @@
 <style>
+   
     .container {
         display: grid;
         grid-template-columns: repeat(3, 1fr);
         gap: 20px;
         max-width: 1200px;
+       
         margin: 0 auto;
     }
     
@@ -30,7 +32,12 @@
     .links div {
         margin-bottom: 10px;
     }
-    
+    .copy {
+        text-align: center;
+        padding-top: 3px;
+        padding-top: 3px;
+        background-color: rgba(221, 221, 221, 0.993);
+    }
     
     
     @media (max-width: 500px) {
@@ -40,7 +47,7 @@
     }
       .container{
         display:flex;
-      padding: 0 4% 0 4%;
+      padding: 0 8% 0 8%;
       }
     
     .infos {
@@ -59,14 +66,10 @@
             <img src="/images/logoNavBar.png" alt="Logo" />
         </div>
         <div class="infos">
-            <div>
-                <a href="{{ url('https://www.google.com/maps/place/26+Bd+Carabacel,+06000+Nice/@43.7042617,7.2743648,17z/data=!3m1!4b1!4m6!3m5!1s0x12cddaafbc1f660f:0x665cc91f30f6b249!8m2!3d43.7042617!4d7.2743648!16s%2Fg%2F11c1klq5c7?entry=ttu') }}" target="_blank">Adresse : 26 boulevard Carabacel 06000 Nice</a>
-            </div>
-            <div>
-                <a href="tel:@php echo '0493624458'; @endphp">0493624458</a>
-            </div>
-    
-            &copy; Nolan Costa & Andréa Baglieri
+                <p><u>Contact :</u></p>
+                <a href="tel:@php echo '0493624458'; @endphp">Téléphone: 0493624458</a></br>
+                <a href="mailto:community@lebocal.academy">community@lebocal.academy</a></br>
+                <a href="{{ url('https://www.google.com/maps/place/26+Bd+Carabacel,+06000+Nice/@43.7042617,7.2743648,17z/data=!3m1!4b1!4m6!3m5!1s0x12cddaafbc1f660f:0x665cc91f30f6b249!8m2!3d43.7042617!4d7.2743648!16s%2Fg%2F11c1klq5c7?entry=ttu') }}" target="_blank">26 boulevard Carabacel 06000 Nice</a>       
         </div>
         <nav class='links'>
             <div>
@@ -81,6 +84,8 @@
             </div>
         </nav>
     </div>
-    
+    <div class='copy'>
+            &copy; Nolan Costa & Andréa Baglieri
+            </div
     </body>
     </html>
